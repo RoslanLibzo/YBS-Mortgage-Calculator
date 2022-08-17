2434f978ed70babad10c434c13bc1cf1088874b0
Roslan Lbzo <roslanlibzo@gmail.com>
2022-08-17 20:09:04 +0300

    Slider Value now updates while sliding slider

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 console.log('JS Connected')
+//Stuff for Opening and Closing window
 const openCalcButton = document.querySelector('.openCalculator');
 const calculatorBody = document.querySelector('.calculator-body');
+
+//Stuff for Slider Input
+const sliderInput = document.getElementById('mortgageTerm');
+const sliderValueText = document.getElementById('sliderValue');
+
+//Update Value according to Slider
+sliderInput.oninput = (()=>{
+    let value = sliderInput.value
+    sliderValueText.innerText = value;
+})
 
